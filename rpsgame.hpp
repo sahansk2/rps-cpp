283a252cfa04b6d223461a4566fdbafaefa58f43
Sahan Kumarasinghe <sahansk2@illinois.edu>
2020-07-14 01:12:43 -0400

    Added testing framework

--- rpsgame.hpp
+++ rpsgame.hpp
@@ -16,17 +16,16 @@ private:
 	std::default_random_engine generator_;
 	std::uniform_int_distribution<int> distribution_;
 public:	
-	rpsChoice getRandomChoice();
-	static int evaluateWinner(rpsChoice first, rpsChoice second);
-
 	RPSGame(int maxGames);
 
 	int runMatch(rpsChoice player1Choice, rpsChoice player2Choice);
+	void reset(int numGames=3);
+	rpsChoice getRandomChoice();
 
 	int player2Score();	
 	int player1Score();
 	int numGames();
-	
-	bool isSetOver();	
-	void reset(int numGames=3);
+	bool isSetOver();
+
+	static int evaluateWinner(rpsChoice first, rpsChoice second);
 };
---
+++ test.cpp
@@ -0,0 +1,10 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+#include "rpsgame.hpp"
+
+TEST_CASE("Testing rock-paper-scissors core") {
+    RPSGame game();
+    SUBCASE("Tests on who wins rock paper scissors.") {
+        
+    }
+}
